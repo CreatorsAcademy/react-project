27495c2101dc57b8eb7db4a15e558de0c0a833a4
Balaji Damarla <balaji3049@gmail.com>
2025-03-13 23:08:07 +0530

    Added a demo project - node-basics

---
+++ node-basics/src/index.js
@@ -0,0 +1,13 @@
+console.log("call from src folder");
+
+console.log(1 + 1); //2
+
+console.log(10 + 20); //30
+
+console.log(10 + "20"); //1020
+
+console.log(10 + 2 + "5" + 3 + 1 + "20"); //1253120
+
+console.log(5 - "1" - "2"); //2
+
+console.log((5 - "1") * "2"); //8
